91ba6beaa43473d7885c7925474c74c044198d3d
Laszlo Puskas <laszlo@banzaicloud.com>
2019-04-02 15:30:58 +0200

    draft doc, dev test skeleton

---
+++ docs/store/store.md
@@ -0,0 +1,14 @@
+### Cloudinfo Store configuration
+
+The Cloudinfo application persists information scraped form cloud providers in a component named 'Store'
+
+This store may be backed by various solutions that can be selected using configuration.
+
+The default Store implementation is an in memory KV store.
+
+Suppored Stores:
+
+#### Redis
+
+
+#### Cassandra
---
+++ internal/app/cloudinfo/cistore/redis_test.go
@@ -0,0 +1,35 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cistore
+
+import (
+	"testing"
+)
+
+// Skeleton for dev testing the redis store
+// use the attached docker-compose.yaml compose file
+func Test(t *testing.T) {
+
+	//cfg := redis.Config{
+	//	Host: "localhost",
+	//	Port: 6379,
+	//}
+	//
+	//ps := NewRedisProductStore(cfg, logur.NewTestLogger())
+	//
+	//// /banzaicloud.com/cloudinfo/providers/amazon/regions/ca-central-1/prices
+	//ps.DeleteVm("amazon", "eks", "eu-central-1")
+
+}
