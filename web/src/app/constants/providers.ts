573b3cf52cb8d0558d8e6e89799741f2e88767af
Gabor Kozma <gabo@kozmagabor.com>
2019-05-08 22:41:47 +0200

    fix provider name

--- web/src/app/constants/providers.ts
+++ web/src/app/constants/providers.ts
@@ -15,7 +15,7 @@
  */
 
 export const PROVIDERS = {
-  amazon: { provider: 'amazon', name: 'Amazon EC2' },
+  amazon: { provider: 'amazon', name: 'Amazon' },
   alibaba: { provider: 'alibaba', name: 'Alibaba Cloud' },
   google: { provider: 'google', name: 'Google Cloud' },
   azure: { provider: 'azure', name: 'Microsoft Azure' },
