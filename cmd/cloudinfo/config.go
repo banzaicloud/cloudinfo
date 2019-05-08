cbfbdb50eac30fa6e065c1f40c283a5fc341b85d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-05-08 15:50:56 +0200

    Add new provider flags

--- cmd/cloudinfo/config.go
+++ cmd/cloudinfo/config.go
@@ -214,40 +214,80 @@ func configure(v *viper.Viper, p *pflag.FlagSet) {
 	_ = v.BindPFlag("providers.amazon.enabled", p.Lookup("amazon-enabled"))
 	_ = v.BindEnv("providers.amazon.enabled", "AMAZON_ENABLED")
 
-	p.String("aws-access-key-id", "", "aws access key id")
-	_ = v.BindPFlag("providers.amazon.accessKeyId", p.Lookup("aws-access-key-id"))
-	_ = v.BindEnv("providers.amazon.accessKeyId", "AWS_ACCESS_KEY_ID")
+	{
+		p.String("aws-access-key-id", "", "aws access key id")
+		f := p.Lookup("aws-access-key-id")
+		f.Deprecated = "use --amazon-access-key-id instead"
+		_ = v.BindPFlag("providers.amazon.accessKeyId", f)
+		p.String("amazon-access-key-id", "", "amazon access key id")
+		_ = v.BindPFlag("providers.amazon.accessKeyId", p.Lookup("amazon-access-key-id"))
+		_ = v.BindEnv("providers.amazon.accessKeyId", "AWS_ACCESS_KEY_ID")
+	}
 
-	p.String("aws-secret-access-key", "", "aws secret access key")
-	_ = v.BindPFlag("providers.amazon.secretAccessKey", p.Lookup("aws-secret-access-key"))
-	_ = v.BindEnv("providers.amazon.secretAccessKey", "AWS_SECRET_ACCESS_KEY")
+	{
+		p.String("aws-secret-access-key", "", "aws secret access key")
+		f := p.Lookup("aws-secret-access-key")
+		f.Deprecated = "use --amazon-secret-access-key instead"
+		_ = v.BindPFlag("providers.amazon.secretAccessKey", f)
+		p.String("amazon-secret-access-key", "", "amazon secret access key")
+		_ = v.BindPFlag("providers.amazon.secretAccessKey", p.Lookup("amazon-secret-access-key"))
+		_ = v.BindEnv("providers.amazon.secretAccessKey", "AWS_SECRET_ACCESS_KEY")
+	}
 
-	p.String(
-		"prometheus-address",
-		"",
-		"http address of a Prometheus instance that has AWS spot "+
-			"price metrics via banzaicloud/spot-price-exporter. "+
-			"If empty, the cloudinfo app will use current spot prices queried directly from the AWS API.",
-	)
-	_ = v.BindPFlag("providers.amazon.prometheusAddress", p.Lookup("prometheus-address"))
-	_ = v.BindEnv("providers.amazon.prometheusAddress", "PROMETHEUS_ADDRESS")
+	{
+		p.String(
+			"prometheus-address",
+			"",
+			"http address of a Prometheus instance that has AWS spot "+
+				"price metrics via banzaicloud/spot-price-exporter. "+
+				"If empty, the cloudinfo app will use current spot prices queried directly from the AWS API.",
+		)
+		f := p.Lookup("prometheus-address")
+		f.Deprecated = "use --amazon-prometheus-address instead"
+		_ = v.BindPFlag("providers.amazon.prometheusAddress", f)
+		p.String(
+			"amazon-prometheus-address",
+			"",
+			"http address of a Prometheus instance that has AWS spot "+
+				"price metrics via banzaicloud/spot-price-exporter. "+
+				"If empty, the cloudinfo app will use current spot prices queried directly from the AWS API.",
+		)
+		_ = v.BindPFlag("providers.amazon.prometheusAddress", p.Lookup("amazon-prometheus-address"))
+		_ = v.BindEnv("providers.amazon.prometheusAddress", "PROMETHEUS_ADDRESS")
+	}
 
-	p.String(
-		"prometheus-query",
-		"avg_over_time(aws_spot_current_price{region=\"%s\", product_description=\"Linux/UNIX\"}[1w])",
-		"advanced configuration: change the query used to query spot price info from Prometheus.",
-	)
-	_ = v.BindPFlag("providers.amazon.prometheusQuery", p.Lookup("prometheus-query"))
-	_ = v.BindEnv("providers.amazon.prometheusQuery", "PROMETHEUS_QUERY")
+	{
+		p.String(
+			"prometheus-query",
+			"avg_over_time(aws_spot_current_price{region=\"%s\", product_description=\"Linux/UNIX\"}[1w])",
+			"advanced configuration: change the query used to query spot price info from Prometheus.",
+		)
+		f := p.Lookup("prometheus-query")
+		f.Deprecated = "use --amazon-prometheus-query instead"
+		_ = v.BindPFlag("providers.amazon.prometheusQuery", f)
+		p.String(
+			"amazon-prometheus-query",
+			"avg_over_time(aws_spot_current_price{region=\"%s\", product_description=\"Linux/UNIX\"}[1w])",
+			"advanced configuration: change the query used to query spot price info from Prometheus.",
+		)
+		_ = v.BindPFlag("providers.amazon.prometheusQuery", p.Lookup("amazon-prometheus-query"))
+		_ = v.BindEnv("providers.amazon.prometheusQuery", "PROMETHEUS_QUERY")
+	}
 
 	// Google config
 	p.Bool("google-enabled", true, "enable google provider")
 	_ = v.BindPFlag("providers.google.enabled", p.Lookup("google-enabled"))
 	_ = v.BindEnv("providers.google.enabled", "GOOGLE_ENABLED")
 
-	p.String("gce-api-key", "", "GCE API key to use for getting SKUs")
-	_ = v.BindPFlag("providers.google.apiKey", p.Lookup("gce-api-key"))
-	_ = v.BindEnv("providers.google.apiKey", "GCE_API_KEY")
+	{
+		p.String("gce-api-key", "", "GCE API key to use for getting SKUs")
+		f := p.Lookup("gce-api-key")
+		f.Deprecated = "use --google-gce-api-key instead"
+		_ = v.BindPFlag("providers.google.apiKey", f)
+		p.String("google-gce-api-key", "", "GCE API key to use for getting SKUs")
+		_ = v.BindPFlag("providers.google.apiKey", p.Lookup("google-gce-api-key"))
+		_ = v.BindEnv("providers.google.apiKey", "GCE_API_KEY")
+	}
 
 	p.String("google-application-credentials", "", "google application credentials location")
 	_ = v.BindPFlag("providers.google.appCredentials", p.Lookup("google-application-credentials"))
